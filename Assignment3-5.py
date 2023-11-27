de6096c69ef5af971637c1c0f3b9218b1b234587
Ryotaro (Rio) Takehara <143859347+suisanki@users.noreply.github.com>
2023-11-27 15:36:50 -0600

    Add files via upload

--- Assignment3-5.py
+++ Assignment3-5.py
@@ -1,8 +1,17 @@
 import random
 import statistics
+import time
         
 def listMaker(seed):
+    if not seed.isnumeric():
+        raise ValueError("Invalid input.")
+        
+    if seed == "0":
+        raise ValueError("List length cannot be 0.")
+        
     returnList = []
+    seed = int(seed)
+    
     for i in range(seed):
         returnList.append(random.randint(0,1000))
         
@@ -21,9 +30,12 @@ def listSorter(inputList):
         return listSorter(lesser) + [pivot] + listSorter(greater)
     
 def medianFinder(inputList):
-    if len(inputList) % 2 == 0:
-        return()
-            
+    midIndex = len(inputList)//2
+    if len(inputList) % 2 == 1:
+        return inputList[midIndex]
+    else:
+        return (inputList[midIndex-1] + inputList[midIndex])/2
+               
 def medianChecker(x,inputList):
     if x == statistics.median(inputList):
         return True
@@ -31,8 +43,39 @@ def medianChecker(x,inputList):
         return False
     
 if __name__ == "__main__":
-    
-     seed = input("Enter length of the list: ")
+    while True:
+        try:
+            seed = input("Enter the length of the list (positive integer): ")
+            start = time.time()
+            
+            initialList = listMaker(seed)
+            
+            sortList = listSorter(initialList)
+            median = medianFinder(sortList)
+            end = time.time()
+            
+            ellapsedTime = round(end-start,3)
+            
+            print(f"Median: {median}")
+            print(f"Median (standard library): {statistics.median(sortList)}")
+            
+            if medianChecker(median,initialList):
+                print("\nYES!! Result match with standard library!")
+                print(f"It took {ellapsedTime} seconds to execute.")
+                
+            else:
+                print("\nNo... Result doesn't match with standard library...")
+                print(f"It took {ellapsedTime} seconds to execute.")
+
+            break
+            
+        except ValueError as msg:
+            print(msg + "\n")
+            
+        except RecursionError:
+            print("List is too long to handle... Try something smaller.\n")
+
+            
      
      
         
